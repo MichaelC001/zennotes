120facf84c65ea022e73558c8470de666060a377
Adib Hanna <adibhanna@gmail.com>
2026-08-17 15:29:02 -0500

    packaging(homebrew): 2.29.0, both mac digests pinned

--- packaging/homebrew/Casks/zennotes.rb
+++ packaging/homebrew/Casks/zennotes.rb
@@ -1,9 +1,9 @@
 cask "zennotes" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.20.2"
-  sha256 arm:   "04aa87c257b7c9e7eae52bef669fd06f8430be16184ca17ddb3b266884ddf5e1",
-         intel: "6a346ba9c2138b04e6e067fdbdc016d8a7222cf0d0c3f94a7c8158f24484e505"
+  version "2.29.0"
+  sha256 arm:   "855cf7f9532cdfe906c66792609ccb6c15c6b3eb47ce92d0b90c01f346fe408f",
+         intel: "dd5c0cb1b662b7d34fafb70aed79f5be6901587de70e7c87b2481a76046b8caa"
 
   url "https://github.com/ZenNotes/zennotes/releases/download/v#{version}/ZenNotes-#{version}-mac-#{arch}.dmg"
   name "ZenNotes"
